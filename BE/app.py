f9224dcc44cec952c7813b12e343be6d1d20d6a9
Jein <venommancer123@gmail.com>
2025-05-07 19:48:51 +0800

    week13

--- BE/app.py
+++ BE/app.py
@@ -71,8 +71,5 @@ def delete_history(id):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Buat tabel jika belum ada
     app.run(debug=True, host="0.0.0.0")
-else:
-    with app.app_context():
-        db.create_all()
